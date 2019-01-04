15fd5b9688d557a49743d30e96bca62971482227
Sous Chili <greatdragone75@gmail.com>
2019-01-05 00:27:49 +0400

    Add controllers and main page(example)

---
+++ News_EF/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace News_EF.Controllers
+{
+    
+    public class HomeController : Controller
+    {
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
