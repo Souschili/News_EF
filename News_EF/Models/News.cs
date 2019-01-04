906ec69d077d1ae1b08d8a620c30bdddd5761beb
Sous Chili <greatdragone75@gmail.com>
2019-01-05 00:09:00 +0400

    Add 2 model class

---
+++ News_EF/Models/News.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace News_EF.Models
+{
+    public class News
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Text { get; set; }
+        public DateTime Date { get; } = DateTime.Now.Date; //по умолчанию задаем текущую дату сетер приватный менять нельзя
+        public List<Comments> ComentsList = new List<Comments>(); //список коментариев
+    }
+}
