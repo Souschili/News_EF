7ef960456a4ee433b2ee3aa687de7745bb7658e1
Sous Chili <greatdragone75@gmail.com>
2019-01-06 12:18:06 +0400

    add dbcontext

---
+++ News_EF/Data/NewsDataEF.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using News_EF.Models;
+
+namespace News_EF.Data
+{
+    public class NewsDataEF:DbContext
+    {
+        public DbSet<News> News { get; set; }
+
+        //Пометка:
+        public NewsDataEF(DbContextOptions<NewsDataEF> options):base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
