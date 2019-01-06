ff88b4f01c161fa78b4747943505914b11133ced
Sous Chili <greatdragone75@gmail.com>
2019-01-07 01:40:42 +0400

    Try to add db

--- News_EF/Controllers/HomeController.cs
+++ News_EF/Controllers/HomeController.cs
@@ -1,18 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using News_EF.Data;
 
 namespace News_EF.Controllers
 {
-    
+
     public class HomeController : Controller
     {
+        private NewsDataEF db;
+        public HomeController(NewsDataEF context)
+        {
+            db = context;
+        }
+
         [Route("")]
         public IActionResult Index()
         {
-            return View();
+            return View( db.News.ToList());
         }
     }
 }
--- News_EF/Startup.cs
+++ News_EF/Startup.cs
@@ -1,19 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using News_EF.Data;
 
 namespace News_EF
 {
     public class Startup
     {
-        
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            // получаем строку подключения из файла конфигурации-> appsetings.json
+            string connection = Configuration.GetConnectionString("DefaultConnection");
+            // добавляем контекст NewDataEF в качестве сервиса в приложение
+            services.AddDbContext<NewsDataEF>(options =>
+                options.UseSqlServer(connection));
             services.AddMvc();
         }
 
