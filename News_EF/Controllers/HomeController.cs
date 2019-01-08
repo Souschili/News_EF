0245b575e7081484cd8e3e139f6ac8f063110f83
Sous Chili <greatdragone75@gmail.com>
2019-01-09 01:44:36 +0400

    Test fake repo to output data

--- News_EF/Controllers/HomeController.cs
+++ News_EF/Controllers/HomeController.cs
@@ -3,16 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using News_EF.Services;
 
 namespace News_EF.Controllers
 {
     
     public class HomeController : Controller
     {
+        private INewsRepository _repository;
+
+        public HomeController(INewsRepository repo)
+        {
+            _repository = repo;
+        }
+
         [Route("")]
         public IActionResult Index()
         {
-            return View();
+            return View(_repository.GetNews());
         }
     }
 }
