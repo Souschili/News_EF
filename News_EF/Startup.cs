6fb0aa94f11f24cccbb03e09f8b7719c1e2b090c
Sous Chili <greatdragone75@gmail.com>
2019-01-09 02:14:47 +0400

    edit startup class

--- News_EF/Startup.cs
+++ News_EF/Startup.cs
@@ -5,17 +5,29 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using News_EF.Data;
 using News_EF.Services;
 
 namespace News_EF
 {
     public class Startup
     {
-        
+        //изменения для подключения бд или работы с уже имеющимся ( спасибо метанит)
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration config)
+        {
+            Configuration = config;
+        }
+
+
         public void ConfigureServices(IServiceCollection services)
         {
-            
+            //получаем строку подключения
+            string connection = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<NewsContext>(options => options.UseSqlServer(connection));
             services.AddMvc();
         }
 
