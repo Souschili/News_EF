f11b901eac6cb6759296e2e4bd53902c6421a8d1
Sous Chili <greatdragone75@gmail.com>
2019-01-09 22:28:33 +0400

    clean and rebuild

--- News_EF/Controllers/HomeController.cs
+++ News_EF/Controllers/HomeController.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using News_EF.Services;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace News_EF.Controllers
 {
-    
+
     public class HomeController : Controller
     {
       
--- News_EF/Startup.cs
+++ News_EF/Startup.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using News_EF.Data;
-using News_EF.Services;
 
 namespace News_EF
 {
